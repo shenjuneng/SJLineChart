ea76eee4d8b66b9314efc0ffc8e9b8c00659b145
沈骏 <shenjuneng@sina.com>
2018-04-04 14:35:10 +0800

    初始化项目

---
+++ SJLineChartDemo/SJLineChartDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SJLineChartDemo
+//
+//  Created by 沈骏 on 2018/4/4.
+//  Copyright © 2018年 沈骏. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ SJLineChartDemo/SJLineChartDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SJLineChartDemo
+//
+//  Created by 沈骏 on 2018/4/4.
+//  Copyright © 2018年 沈骏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
